49b48fdcb2f9f7a364a73e4a8b183be811321068
gmarvis <sgmarvis@gmail.com>
2023-10-22 21:32:44 +0100

    get user with token

--- src/purchased-course/purchased-course.service.ts
+++ src/purchased-course/purchased-course.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { PurchasedCourse } from './schemas/purchaseed-course.schema';
 import mongoose from 'mongoose';
+// import { User } from 'src/auth/schemas/user.schema';
+// import { CourseService } from 'src/course/course.service';
 
 @Injectable()
 export class PurchasedCourseService {
@@ -9,4 +11,10 @@ export class PurchasedCourseService {
     @InjectModel(PurchasedCourse.name)
     private purchaseCourseModel: mongoose.Model<PurchasedCourse>,
   ) {}
+
+  // create purcahase course
+  // async createPurchaseCourse(id: string, user: User): Promise<PurchasedCourse>{
+  //   const courseService = CourseService
+  //   const
+  // }
 }
--- src/purchased-course/schemas/purchaseed-course.schema.ts
+++ src/purchased-course/schemas/purchaseed-course.schema.ts
@@ -1,14 +1,17 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose from 'mongoose';
+import { User } from 'src/auth/schemas/user.schema';
+import { Course } from 'src/course/schemas/course.schema';
 
 @Schema({
   timestamps: true,
 })
 export class PurchasedCourse {
-  @Prop()
-  userId: string;
+  @Prop({ type: mongoose.Schema.Types.String })
+  userId: User;
 
-  @Prop()
-  courseId: string;
+  @Prop({ type: mongoose.Schema.Types.String })
+  purchasedCourse: Course;
 }
 
 export const PurchasedCourseSchema =
